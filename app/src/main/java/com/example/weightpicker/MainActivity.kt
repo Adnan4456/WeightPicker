2945601aa985ba2b4abd79569cc801930e94fa14
adnan <adnan4456@gmail.com>
2024-04-09 18:12:58 +0200

    first commit

--- app/src/main/java/com/example/weightpicker/MainActivity.kt
+++ app/src/main/java/com/example/weightpicker/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.weightpicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.weightpicker.ui.theme.WeightPickerTheme
 
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.weightpicker.ui.theme.WeightPickerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DrawingScreen()
                 }
             }
         }
@@ -30,14 +37,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DrawingScreen(){
+
+    val lines = remember {
+        mutableStateListOf<Line>()
+    }
+
+    val scope = rememberCoroutineScope()
+
+    Canvas(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(true) {
+            detectDragGestures { change, dragAmount ->
+                change.consume()
+
+                val line = Line(
+                    start = change.position - dragAmount,
+                    end = change.position
+                )
+                lines.add(line)
+            }
+        }
+    ){
+        lines.forEach{line ->
+
+            drawLine(
+                color = line.color,
+                start = line.start,
+                end = line.end,
+                strokeWidth = line.strokeWidth.toPx()
+            )
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WeightPickerTheme {
-        Greeting("Android")
-    }
-}
+data class Line(
+    val start : Offset,
+    val end : Offset,
+    val color : Color= Color.Black,
+    val strokeWidth: Dp = 1.dp
+    )
