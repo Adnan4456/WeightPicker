0532c3f563c4809a0a9c476837d75ab83239d86e
adnan <adnan4456@gmail.com>
2024-04-10 21:38:34 +0200

    first commit

--- app/src/main/java/com/example/weightpicker/MainActivity.kt
+++ app/src/main/java/com/example/weightpicker/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeightPickerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -43,7 +42,6 @@ fun DrawingScreen(){
         mutableStateListOf<Line>()
     }
 
-    val scope = rememberCoroutineScope()
 
     Canvas(modifier = Modifier
         .fillMaxSize()
