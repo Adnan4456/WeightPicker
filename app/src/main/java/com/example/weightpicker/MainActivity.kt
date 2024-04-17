ed7dcf37a4bc4e065f9980daf37fea10a314b897
adnan <adnan4456@gmail.com>
2024-04-17 23:39:21 +0200

    circle is done

--- app/src/main/java/com/example/weightpicker/MainActivity.kt
+++ app/src/main/java/com/example/weightpicker/MainActivity.kt
@@ -3,45 +3,21 @@ package com.example.weightpicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Settings
+
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weightpicker.ui.theme.WeightPickerTheme
-
-data class  BottomNavigationItem(
-    val title: String,
-    val selectedIcon : ImageVector,
-    val unSelectedIcon : ImageVector,
-
-)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,15 +29,29 @@ class MainActivity : ComponentActivity() {
                 mutableStateOf(80)
             }
             WeightPickerTheme {
-                Box(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ){
+                    Row {
+                        Text(text = "$weight",
+                            style = TextStyle(
+                                fontSize = 34.sp,
+                                color = Color.Black
+                            )
+                        )
+                        Text(text = "KG",
+                        textAlign = TextAlign.End)
+                    }
+
+
+                    Spacer(modifier = Modifier.height(50.dp))
                     Scale(
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(300.dp)
-                            .align(Alignment.Center)
+//                            .align(Alignment.Center)
                     ) {
 
                         weight = it
--- app/src/main/java/com/example/weightpicker/WeightElements.kt
+++ app/src/main/java/com/example/weightpicker/WeightElements.kt
@@ -13,10 +13,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.draw
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.text.drawText
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.withRotation
 import kotlin.math.*
